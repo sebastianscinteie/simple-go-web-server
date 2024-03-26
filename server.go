5d7a6880ad404f3df74946586dc22f2f4e989e8b
sebastianscinteie <sebastian.scinteie@gmail.com>
2024-03-26 19:30:50 +0200

    Increment

--- server.go
+++ server.go
@@ -5,12 +5,27 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func increment(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+
+	mutex.Unlock()
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
+
+	http.HandleFunc("/increment", increment)
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi!")
